2c9ee9e38928edf03ced5b79e7cb883e69b46a82
Ahmed <atomcgarry@gmail.com>
2022-07-16 12:16:26 +0100

    test without .env

--- src/hooks/useReed.ts
+++ src/hooks/useReed.ts
@@ -24,7 +24,7 @@ const useReed = ({ title , location , page }: props) => {
         try {
           const options = {
             method: "GET",
-            url: `${process.env.SERVER_API}`,
+            url: `https://jobhack2.herokuapp.com/api/reed`,
             // params: { title, location, page },
             headers: {
               "Content-Type":"application/json",
