fe2ca8331f54ac436a34ca1b928498e9bc85f264
Ahmed McGarry <atomcgarry@gmail.com>
2022-09-01 12:26:29 +0100

    delete a user

--- src/utils/deleteUser.ts
+++ src/utils/deleteUser.ts
@@ -1,3 +1,27 @@
-const deleteUser = (id: number) => {}
+import axios from "axios"
+
+const deleteUser = async (email: string) => {
+    try {
+        const options = {
+            url: "http://localhost:5001/api/user",
+            method: "DELETE", 
+            headers: {
+                "Content-Type":"application/json"
+            },
+            data: {
+                email: email
+            }
+        }
+
+        console.log(email)
+
+        const data = await axios.request(options)
+        const response = data.status === 200 ? true : false
+        return response
+    } catch (error) {
+        console.error(error)
+        return false
+    }
+}
 
 export default deleteUser
